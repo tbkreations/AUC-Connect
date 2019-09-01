f749ae93fce4420653aaac3b2894a34e8385e8ff
Grant <goalscorer22@yahoo.com>
2019-09-01 16:31:05 -0400

    Grant Changes

--- GitHub/aucConnect/public/js/client.js
+++ GitHub/aucConnect/public/js/client.js
@@ -1,33 +1,58 @@
-$('#form-submit').on('click', function(e) {
-    e.preventDefault(); // prevent the button from refreshing the page
+$('#register').on('click', function(e) {
     var userEmail = $('#user-email');
+    var nodemailer = require('nodemailer'); 
+    var transporter = nodemailer.createTransport({ 
+	    service: 'gmail'; 
+	    auth: {
+	      user: 'grantflash@gmail.com', 
+	      pass: 'MoTu&2e4'
+            }
+    )}; 
+    
+    var mailOptions = { 
+	    from: 'grantflash@gmail.com', 
+	    to: userEmail, 
+	    subject: 'Welcome to AUC Connect!', 
+	    text: 'Click on the link to confirm your registration'; 
+    }; 
 
-    if (userEmail.val().indexOf('morehouse.edu') !== -1) { // validation
 
-    } else if (userEmail.val().indexOf('spelman.edu') !== -1) { // validation
-
-    } else if (userEmail.val().indexOf('cau.edu') !== -1) {
-
-    } else {
-        $('#submission-info').text('Sorry, this platform is for AUC students only. You must sign up with your school email. Please try again.');
-    }
-    // this doesn't mean .com or .org is at the end
-    // you may want to check that by using a regular expression if necessary        
-
-    alert("Validation Works"); 
-
-
-    var formData = $('#contact-form').serialize(); // This gets the <form id="contact-form"> element's values and serializes it into a string.
-
- $.ajax({
-        url: 'mail.php', // make sure this file is either in the same directory or the path is changed
-        type: 'POST',
-        data: formData
-    }).done(function(response) {
-        // do stuff to to show the user that the form was submitted
-        $('#submission-info').text('Success! Please validate your email and you can begin connecting with the AUC!');
-    }).fail(function(response, error) {
-        // tell the user what happened that caused the form submission to fail
-        $('#submission-info').text('Oh no, something has happened! Please try again.');
+    e.preventDefault(); // prevent the button from refreshing the page
+    var accceptable = ["morehouse.edu", "spelman.edu", "cau.edu];  
+    if (userEmail.val().split('@')[1] === acceptable[0])  { // Morehouse validation
+	alert("Morehouse Email");
+	transporter.sendMail (mailOptions, function(error, info) {
+            if(error) {
+                    console.log("Error Sending Email");
+            }else{ 
+                    console.log("Email sent: " + info.response);
+            }
     });
+
+    } else if (userEmail.val().split('@')[1] === acceptable[1]) { // Spelman validation
+	alert("Spelman Email");
+	transporter.sendMail (mailOptions, function(error, info) {
+            if(error) {
+                    console.log("Error Sending Email");
+            }else{ 
+                    console.log("Email sent: " + info.response);
+            }
+    });
+
+    } else if (userEmail.val().split('@')[1] === acceptable[2]) { // CAU validation
+	alert("CAU Email"); 
+	transporter.sendMail (mailOptions, function(error, info) {
+            if(error) {
+                    console.log("Error Sending Email");
+            }else{ 
+                    console.log("Email sent: " + info.response);
+            }
+    });
+    
+    } else { 
+        alert("Sorry, this platform is for AUC students only. You must sign up with your school email. Please try again.");
+    }        
+
+}     
+
 
